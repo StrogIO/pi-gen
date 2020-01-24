7203829b38ecbc083ee17cf18c743f7bddac8508
Alex Stromberg <alexstromberg@MacBook-Pro-Alex.local>
2020-01-24 19:55:16 +0200

    removed comitup.conf

--- stage2/04-comitup/00-run.sh
+++ stage2/04-comitup/00-run.sh
@@ -7,8 +7,6 @@ install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
 
 install -m 644 files/comitup.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
-cp -i -n files/comitup.conf ${ROOTFS_DIR}/etc/
-
 on_chroot apt-key add - < files/key-366150CE.pub.txt
 on_chroot << EOF
 apt-get update
