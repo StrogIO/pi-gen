8055a206f689cdf4accb3c43f8c6276d8ad24e4c
Alex Stromberg <alexstromberg@MacBook-Pro-Alex.local>
2020-01-24 19:38:50 +0200

    fix for copying file

--- stage2/04-comitup/00-run.sh
+++ stage2/04-comitup/00-run.sh
@@ -7,7 +7,7 @@ install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
 
 install -m 644 files/comitup.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
 
-cp files/comitup.conf ${ROOTFS_DIR}/etc/
+cp -i -n files/comitup.conf ${ROOTFS_DIR}/etc/
 
 on_chroot apt-key add - < files/key-366150CE.pub.txt
 on_chroot << EOF
